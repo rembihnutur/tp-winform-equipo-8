914e67726f8a546651abc8d1685b453224fd8b91
Pedro Galindez <pedrodgj1497@gmail.com>
2023-09-09 15:40:51 -0300

    creacion de Clases principales para manejo de la aplicacion

---
+++ TPWinForm_equipo-8/dominio/Articulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Articulo
+    {
+        public string Categoria { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int IdMarca { get; set; }
+        public int IdCategoria { get; set; }
+        public List<Imagen> Imagenes { get; set; } 
+    }
+}
